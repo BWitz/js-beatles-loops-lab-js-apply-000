96703cbf8aa3f1f734469b08517390602cd217f2
BWitz <bhorowitz2014@gmail.com>
2018-09-07 06:44:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ function theBeatlesPlay(musicians, instruments) {
   return theBeatlesPlayThese;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
-
-// function johnLennonFacts(facts)
+function johnLennonFacts(facts) {
+  let i = 0;
+  var newJLF = [];
+  while(facts.length > i) {
+    newJLF.push(`${johnLennonFacts[i]}!!!`)
+    i++
+  }
+  return newJLF;
+}
 
 
 
