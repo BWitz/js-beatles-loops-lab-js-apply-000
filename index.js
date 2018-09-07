409550112f91b6055b8938888f0afeba62da34c5
BWitz <bhorowitz2014@gmail.com>
2018-09-07 06:03:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+var musicians = ["John Lennon", "Ringo Starr", "George Harrison", "Paul McCartney"];
+var instruments = ["plays the Rhythm Guitar", "plays the Drums", "plays the Lead Guitar", "plays the Bass Guitar"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatlesPlayThese = [];
+  for (let i = 0; i < musicians.length; i++) {
+    (let p = 0; p < instruments.length; p++) {
+      theBeatlesPlayThese.push(musicians[i] + instruments[p]);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+var theBeatles = ["John Lennon", "Ringo Starr", "George Harrison", "Paul McCartney"];
+var instrumentPlayed = ["plays the Rhythm Guitar", "plays the Drums", "plays the Lead Guitar", "plays the Bass Guitar"];
