d56b4a880a9db42257720ef0b246d4e522c62e3e
BWitz <bhorowitz2014@gmail.com>
2018-09-07 06:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ var instruments = ["Rhythm Guitar", "Drums", "Lead Guitar", "Bass Guitar"];
 function theBeatlesPlay(musicians, instruments) {
   var theBeatlesPlayThese = [];
   for (let i = 0; i < 4; i++) {
-      theBeatlesPlay.push(`${musicians[0]} ${instruments[0]);
+      theBeatlesPlay.push(`$)
 }
-
-
+}
 
 
 
